98997948f1c29e28e60b5d043f27989f8b20f9ab
Fernando Escalante <fernandoescalante1996@hotmail.com>
2023-05-31 22:33:10 -0600

    Mi primer archivo de repaso

---
+++ repaso.py
@@ -0,0 +1,47 @@
+# Comentariando
+print('hola script 1')
+# algo
+print(8/3)
+
+# Test 1
+name = "Fernando"
+last_name = "Escalante"
+quote = "I'm "
+print(quote, name, last_name)
+var1 = 1
+# var1 = input("Ingresa var1 ")
+# print(f"{name} is {not (var1 + True)}" )
+
+# if var1 == "1":
+# print("hola mundo")
+
+lista1 = [1, 2, 3, 4, 5]
+print(lista1)
+print(type(lista1))
+lista1.pop()
+print(lista1)
+#CRUD create read update delete para listas
+
+# append(): Añade un ítem al final de la lista.
+# clear(): Vacía todos los ítems de una lista.
+# extend(): Une una lista a otra.
+# count(): Cuenta el número de veces que aparece un ítem.
+# index(): Devuelve el índice en el que aparece un ítem (error si no aparece).
+# insert(): Agrega un ítem a la lista en un índice específico.
+# pop(): Extrae un ítem de la lista y lo borra.
+# remove(): Borra el primer ítem de la lista cuyo valor concuerde con el que indicamos.
+# reverse(): Le da la vuelta a la lista actual.
+# sort(): Ordena automáticamente los ítems de una lista por su valor de menor a mayor.
+
+# las tuplas no se pueden modificar
+# si se puede pasar de tupla a lista y viceversa
+#  con la instruccion, mi_lista = list(tupla1) o mi_tupla = tuple(lista1)
+# [ ] = Listas
+# ( ) = Tuplas
+# { } = Diccionarios
+# print( "fer" in name)
+# print((8 / 2) + 4 * 8)
+# print(8000 > 3330)
+primos = [2,3,5,7,11,13,17,19,23,29,31,27,41]
+print(primos[3])
+print( 4 != 10)
