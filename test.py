0dae181d4e555f1859e9befb485ffe0b91d00639
Fernando Escalante <fernandoescalante1996@hotmail.com>
2023-05-31 22:51:19 -0600

    Actualizar datos

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("hola desde python")
+import matplotlib.pyplot as plt
+
+def generate_pie_plot():
+    labels = [ 'A', 'B', 'C' ]
+    values = [ 200, 34, 120]
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels)
+    plt.show()
+
+generate_pie_plot()
